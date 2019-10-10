3308ab11ec759a84c93d47c16cbdbc268c8affa3
Dan Mihai Gocan <dan.m.gocan@workdayinternal.com>
2019-10-10 10:23:02 +0100

    Fixed a broken function in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,14 @@
-$(document).ready(function() {
-  $("#coin-space-calculator").on("click", function() {
-    return false;
-  });
-
-  $("#mad-memory-game").on("click", function() {
-    return false;
-  });
-
-  $("#the-brigades").on("click", function() {
-    return false;
-  });
-
-  $("#social-network").on("click", function() {
-    return false;
-  });
 
   /* Function taken from StackOverflow and modified.
   Source: https://stackoverflow.com/questions/9383016/for-loop-at-an-interval */
   function eloper() {
-    $("#empty-line").remove();
+    var eloperElement = document.getElementById("eloper");
     let interval;
     let i = 0;
     let textInitial = "Full Stack Developer";
     let text = textInitial.split("");
     function dostuff() {
-      $("#eloper").append(text[i]);
+      eloperElement.append(text[i]);
 
       if (i < text.length) i++;
       else clearInterval(interval);
@@ -35,9 +19,3 @@ $(document).ready(function() {
 
   eloper();
 
-  if ($(window).width() < 480) {
-    $("input").addClass("form-control");
-  }
-});
-
-/* jQuery media */
